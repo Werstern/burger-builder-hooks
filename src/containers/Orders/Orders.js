8252764c2c1379d2d0f0f838c9de692113a0b70e
Werstern <werstern@bigmir.net>
2019-03-09 00:56:02 +0200

    Adjusting the order & checkout containers

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
@@ -7,17 +7,16 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
-class Orders extends Component {
+const orders = props => {
 
-  componentDidMount() {
-    this.props.onFetchOrders(this.props.token, this.props.userId);
-  }
+  useEffect(() => {
+    props.onFetchOrders(props.token, props.userId);
+  }, []);
 
-  render() {
     let orders = <Spinner />;
-    if(!this.props.loading) {
+    if(!props.loading) {
       orders =
-        this.props.orders.map(order => (
+        props.orders.map(order => (
           <Order
             key={order.id}
             ingredients={order.ingredients}
@@ -29,7 +28,6 @@ class Orders extends Component {
         {orders}
       </div>
     );
-  }
 }
 
 const mapStateToProps = state => {
@@ -47,4 +45,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(orders, axios));
