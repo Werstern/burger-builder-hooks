afbca5dcb062e5c5c0951213bcb4e2a79f96ba16
Werstern <werstern@bigmir.net>
2019-03-08 22:56:06 +0200

    converting <App> with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import asyncComponent from './hoc/asyncComponent/asyncComponent';
@@ -20,12 +20,12 @@ const asyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
+const app = props => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
 
-  render() {
+
     let routes = (
       <Switch>
         <Route path="/auth" component={asyncAuth} />
@@ -33,7 +33,7 @@ class App extends Component {
         <Redirect to="/" />
       </Switch>
     );
-    if (this.props.isAuthenticated) {
+    if (props.isAuthenticated) {
       routes = (
         <Switch>
           <Route path="/checkout" component={asyncCheckout} />
@@ -52,7 +52,6 @@ class App extends Component {
         </Layout>
       </div>
     );
-  }
 }
 
 const mapStateToProps = state => {
@@ -67,4 +66,9 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(app)
+);
